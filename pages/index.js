138fad72fb2e927fe2a08d8676055d607541bbda
karargee <kararzoe9@gmail.com>
2026-01-11 09:47:07 +0100

    Add Home component with styled content

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>TshungKath</h1>
+      <p>Your Kinky Trans Dominatrix & Sissy Trainer</p>
+      <style jsx>{`
+        div {
+          padding: 20px;
+          text-align: center;
+        }
+        h1 {
+          color: #ff1493;
+        }
+      `}</style>
+    </div>
+  )
+}
