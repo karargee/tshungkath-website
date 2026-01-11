ff6ae5d3645518c1e2f4919148063a3bc5ff44b0
karargee <kararzoe9@gmail.com>
2026-01-11 10:04:09 +0100

    Add Home component to index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding: '20px', textAlign: 'center', background: '#000', color: '#fff', minHeight: '100vh'}}>
+      <h1 style={{color: '#ff1493'}}>TshungKath</h1>
+      <p>Your Kinky Trans Dominatrix & Sissy Trainer</p>
+      <p>Website is loading...</p>
+    </div>
+  )
+}
